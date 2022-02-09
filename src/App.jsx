dcdfddf76417ca18785e8cefff3d19c4061ec867
orian <77785778+OrianP@users.noreply.github.com>
2022-02-09 12:10:36 +0000

    Project setup and linting config
    Co-authored-by: Adam <31065348+adam8-9@users.noreply.github.com>

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  // const [count, setCount] = React.useState(0)
+
+  return (
+    <div className="App">
+      Hello
+    </div>
+  )
+}
+
+export default App
