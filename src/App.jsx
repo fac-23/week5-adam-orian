acdb5ebd4086f9feaeb40a83f2761e26723f60e7
orian <77785778+OrianP@users.noreply.github.com>
2022-02-09 18:16:02 +0000

    Add num variable and return Enemy components
    Co-authored-by: Adam <31065348+adam8-9@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Form from "./components/Form.jsx";
 import Pokemon from "./components/Pokemon.jsx";
+import Enemy from "./components/Enemy.jsx";
 
 function App() {
 	const [pokemonName, setPokemonName] = React.useState(null);
-	console.log(pokemonName);
+	const [num, setNum] = React.useState(null);
+	// console.log(pokemonName);
 	return (
 		<div>
-			<Form pokemonName={pokemonName} setPokemonName={setPokemonName} />
+			<Form
+				pokemonName={pokemonName}
+				setPokemonName={setPokemonName}
+				setNum={setNum}
+			/>
 			<Pokemon name={pokemonName} />
+			<Enemy num={num} />
+			<Enemy num={num} />
 		</div>
 	);
 }
