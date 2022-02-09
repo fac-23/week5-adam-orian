d5747bd9a6f514cae80c2102a85d5b923d83c898
orian <77785778+OrianP@users.noreply.github.com>
2022-02-09 13:14:24 +0000

    Create folder for components. Create initial Form component
    Co-authored-by: Adam <1065348+adam8-9@users.noreply.github.com>

---
+++ src/components/Form.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+// form with:
+// username input
+// pokemon input (to type pokemon name) - track state of pokemon name
+// button to generate random pokemon
+
+// eslint-disable-next-line no-unused-vars
+function Form({ pokemonName, setPokemonName }) {
+	return (
+		<form
+			onSubmit={(event) => {
+				event.preventDefault();
+				setPokemonName(event.target.pokemonName.value);
+			}}
+		>
+			<label htmlFor="username">
+				Username
+				<input type="text" id="username" name="username" />
+			</label>
+			<label htmlFor="pokemonName">
+				Type a Pokemon name
+				<input type="text" id="pokemonName" name="pokemonName" />
+			</label>
+			<button type="submit">Go!</button>
+		</form>
+	);
+}
+
+export default Form;
