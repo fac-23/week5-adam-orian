04abca83fdc2800b364d79d4faed7eae2732ad9b
orian <77785778+OrianP@users.noreply.github.com>
2022-02-09 16:02:36 +0000

    Add inline styles for positioning sprite
    Co-authored-by: Adam <31065348+adam8-9@users.noreply.github.com>

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -10,12 +10,30 @@ function Pokemon({ name }) {
 		fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
 			.then((data) => data.json())
 			.then((data) => setSprite(data.sprites.front_default));
+
+		// function handleKeyDown(event){
+		//     if(event.key === "ArrowUp") {
+
+		//     }
+		// }
 	});
 
 	if (!sprite) {
 		return <div>Loading...</div>;
 	} else {
-		return <img src={sprite} />;
+		return (
+			<div className="spriteContainer">
+				<img
+					className="sprite"
+					src={sprite}
+					style={{
+						bottom: "0vh",
+						left: "50vw",
+						transform: "translate(-50%, -50%)",
+					}}
+				/>
+			</div>
+		);
 	}
 }
 
