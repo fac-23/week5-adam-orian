4cd6a3e860ca9653a57827e6f7dd5fdf6738f808
orian <77785778+OrianP@users.noreply.github.com>
2022-02-09 14:35:31 +0000

    Add pokemon component
    Co-authored-by: Adam <31065348+adam8-9@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Form from "./components/Form.jsx";
+import Pokemon from "./components/Pokemon.jsx";
 
 function App() {
 	const [pokemonName, setPokemonName] = React.useState(null);
-  console.log(pokemonName);
+	console.log(pokemonName);
 	return (
 		<div>
-			<Form pokemonName={pokemonName} setPokemonName={setPokemonName}/>
-			{pokemonName}
+			<Form pokemonName={pokemonName} setPokemonName={setPokemonName} />
+			<Pokemon name={pokemonName} />
 		</div>
 	);
 }
